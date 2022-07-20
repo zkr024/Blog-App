fa9fb629fca6347a123cfde0e2b7eacce880e3de
zkr024 <zkr.bm.02403@gmail.com>
2022-07-20 17:59:47 -0500

    complete plain views

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   resources :users, only: [:index, :show] do
-    resources :posts, only: [:index, :show]
+    resources :posts, only: [:index, :show] do
+      resources :comments, only: [:index, :show]
+    end
   end
 end
