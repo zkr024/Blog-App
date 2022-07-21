01b894a86446a03bafe46895d12b99503abdc6c7
zkr024 <zkr.bm.02403@gmail.com>
2022-07-21 01:58:22 -0500

    root

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get "/", to: "main#index"
+
   resources :users, only: [:index, :show] do
     resources :posts, only: [:index, :show] do
       resources :comments, only: [:index, :show]
