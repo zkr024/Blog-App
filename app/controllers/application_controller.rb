97617cb1351d00198ccfb2204767897522aae083
Aduda-Boaz <adudaboaz@gmail.com>
2022-07-29 20:34:29 +0300

    modify the application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,13 @@
-class ApplicationController < ActionController::API
+class ApplicationController < ActionController::Base
   before_action :authenticate_user!
+  protect_from_forgery prepend: true
+
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:name, :photo, :bio, :email, :password) }
+    devise_parameter_sanitizer.permit(:account_update) { |u| u.permit(:name, :email, :password, :current_password) }
+  end
 end
